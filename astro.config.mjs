679d14640a3dcfff560a8d31d75d2ff136fc8b2d
Diego Gómez Díaz <diego.gomez@escuelaideo.edu.es>
2022-02-28 21:27:10 +0100

    set astro config parameters

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,10 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	// Comment out "renderers: []" to enable Astro's default component support.
 	renderers: [],
+	public: 'static',
+	dist: 'public',
+	buildOptions: {
+		sitemap: true,
+		site: 'https://github-k7td6t-i8mlyk.vercel.app/',
+	},
 });
